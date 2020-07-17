2187a720288a23236d81882dd233dc56faa45cfb
Ayushman Chakravarty <ayushmanchak@gmail.com>
2020-07-17 14:53:42 +0530

    Final Commit, Cheers!!

--- LiveWeatherFinal.py
+++ LiveWeatherFinal.py
@@ -1,6 +1,7 @@
 import requests, json
 import xlwings as xw
 import time
+import sys
 
 
 def get_temperature(city_name):
@@ -23,41 +24,47 @@ def kelvin_to_farenheit(tmp):
     return round(f,2)
 
 def weather_xlsx():
-    workbook = xw.Book('D:\PROGRAMMING\Python\Internship\LiveWeather-Updated')
+    workbook = xw.Book('/Users/ASUS/Desktop/AyushWeatherAssignment/weather.xlsx')
     live = workbook.sheets['Sheet1']
 
     cities = workbook.sheets['Sheet2']
 
+    print("Presss 'ctrl+c' to end the script! ")
     while True:
+        
         total_cities = workbook.sheets[0].range('A' + str(workbook.sheets[0].cells.last_cell.row)).end('up').row
         for i in range(2, total_cities + 1):
             try:
-                
                 city = live.range('A' + str(i)).value
                 data = get_temperature(city)
-
-                
                 if live.range('D' + str(i)).value == 1:
-                    if live.range('C' + str(i)).value == 'C':
+                    if live.range('C' + str(i)).value == 'C' or live.range('C' + str(i)).value == 'c':
                         live.range('B' + str(i)).value = str(kelvin_to_celsius(data[0])) + 'C'
-                    elif live.range('C' + str(i)).value == 'F':
+                    elif live.range('C' + str(i)).value == 'F' or live.range('C' + str(i)).value == 'f':
                         live.range('B' + str(i)).value = str(kelvin_to_farenheit(data[0])) + 'F'
                         
-                cities.range('C'+str(i)).value = str(kelvin_to_celsius(data[0])) + 'C'       
+                
                 cities.range('A'+str(i)).value = data[1]
                 cities.range('B'+str(i)).value = data[2]
                 cities.range('D'+str(i)).value = data[3]
+
+                cities.range('C'+str(i)).value = 'Auto Refresh'
                 time.sleep(0.5)
-                cities.range('C'+str(i)).value = str(kelvin_to_farenheit(data[0])) + 'F'
-                
+                cities.range('C'+str(i)).value = str(kelvin_to_farenheit(data[0])) + 'F' +'/'+ str(kelvin_to_celsius(data[0])) + 'C'
+            
             except KeyError:
-
-                live.range('B'+str(i)).value = 'Not found'
-
+                live.range('B'+str(i)).value = 'City Not found'
+            
             except TypeError:
+                live.range('B'+str(i)).value = 'City Not found'
+            
+            except KeyboardInterrupt:
+                print("Thank you for using this Weather Automation Script")
+                sys.exit()
+            
 
-                live.range('B'+str(i)).value = 'no city name'
-            time.sleep(1) #updates value every 1 seconds
+
+        time.sleep(0.5) #updates value every 1 seconds
 
 
 weather_xlsx()
